241ca7f0f9b26c29f4a32eb999d065c6a50a03e6
toonpenen <toon.penen@student.intecbrussel.be>
2021-04-30 23:23:11 +0200

    Add project files.

---
+++ MultiTool/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom bij de multitool applicatie. Maak uw keuze:");
+            Console.WriteLine("1.Rekenmachine");
+            Console.WriteLine("2.Password tester");
+            Console.WriteLine("3.Recyclage");
+            Console.WriteLine("4.Computersolver");
+            int keuze = Convert.ToInt32(Console.ReadLine());
+            switch (keuze)
+            {
+                case 1:
+                    //Hier rekenmachine code
+                    Console.WriteLine("Geef getal 1");
+                    int getal1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Geef getal 2");
+                    int getal2 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Welke bewerking wil je doen? (+,-,*,/,%)");
+                    char keuzebew = Convert.ToChar(Console.ReadLine());
+                    double result = 0;
+                    if (keuzebew == '+')
+                    { result = getal1 + getal2; }
+                    else if (keuzebew == '-')
+                    { result = getal1 - getal2; }
+                    else if (keuzebew == '*')
+                    { result = getal1 * getal2; }
+                    else if (keuzebew == '/')
+                    { result = getal1 / getal2; }
+                    else if (keuzebew == '%')
+                    { result = getal1 % getal2; }
+
+                    if (result < 0)
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine($"Uitkomst is:{result}");
+                    break;
+                case 2:
+                    //Hier password tester code
+                    Console.WriteLine("Geef paswoord");
+                    const string MAINPASS = "TrumpSux";
+                    string inputPass = Console.ReadLine();
+                    if (inputPass == MAINPASS)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Toegelaten");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Verboden");
+                    }
+                    break;
+                case 3:
+                    //Hier recyclage code
+                    break;
+                case 4:
+                    //Hier computersolver code
+                    Console.WriteLine("Does the computer turn on?(y/n)");
+                    string inp = Console.ReadLine();
+                    if (inp == "y")
+                    {
+                        Console.WriteLine("Are there any messages?");
+                        inp = Console.ReadLine();
+                        if (inp == "y")
+                        {
+                            Console.WriteLine("Perform a search for the error message");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Computer is fine");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Is the computer light on?");
+                        inp = Console.ReadLine();
+                        if (inp == "y")
+                        {
+                            Console.WriteLine("Turn the computer monitor on");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Check the computer power cord");
+                        }
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Onbekend keuze ingevoerd. Voer getal tussen 1 en 4 in");
+                    break;
+            }
+        }
+    }
+}
---
+++ Quarantaine/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quarantaine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MIN_DAG = 1;
+            const int MAX_DAG = 5;
+
+            Console.WriteLine("Wat wil je doen? Covidbeslissingshelper (c) of covidquiz (q)");
+            char keuze = char.Parse(Console.ReadLine());
+            switch (keuze)
+            {
+                case 'q':
+                    Random generator = new Random();
+                    int vraagGetal = generator.Next(MIN_DAG, MAX_DAG + 1);
+                    Console.WriteLine($"Je bent al {vraagGetal} dagen in quarantaine");
+                    Console.WriteLine("Hoeveel dagen moet je wachten voor je getest moet worden?");
+                    int antwoordGebruiker = int.Parse(Console.ReadLine());
+                    int oplossing = MAX_DAG - vraagGetal;
+                    if (antwoordGebruiker == oplossing)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Correct");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Fout");
+                    }
+                    break;
+                case 'c':
+                    bool moetInQuarantine = false;
+                    Console.WriteLine("Vertoon je covid symptomen?(j/n)");
+                    string symptomenVraag = Console.ReadLine();
+                    if (symptomenVraag == "n")
+                    {
+                        Console.WriteLine("In nauw contact gekomen met iemand die ziek is?(j/n)");
+                        string nauwcontactVraag = Console.ReadLine();
+                        if (nauwcontactVraag == "n")
+                        {
+                            Console.BackgroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Je mag naar school blijven gaan");
+                        }
+                        else
+                            moetInQuarantine = true;
+                    }
+                    if (symptomenVraag == "j" || moetInQuarantine)
+                    {
+                        Console.WriteLine("Ga in quarantaine en laat je testen");
+                        Console.WriteLine("Heb je een positieve test?(j/n)");
+                        string posTestGebruiker = Console.ReadLine();
+                        if (posTestGebruiker == "j")
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.WriteLine("7 dagen quarantine");
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Naar school wanneer genezen");
+                        }
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Verkeerde invoer");
+                    break;
+            }
+        }
+    }
+}
